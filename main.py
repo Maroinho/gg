d95468826942ee006391a335358fba54920aa75c
unknown <bendemran@gmail.com>
2022-05-06 20:45:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import rsa
+
+
+def generateKeys():
+    (publicKey, privateKey) = rsa.newkeys(1024)
+    with open('keys/publicKey.pem', 'wb') as p:
+        p.write(publicKey.save_pkcs1('PEM'))
+    with open('keys/privateKey.pem', 'wb') as p:
+        p.write(privateKey.save_pkcs1('PEM'))
+
+
+def loadKeys():
+    with open('keys/publicKey.pem', 'rb') as p:
+        publicKey = rsa.PublicKey.load_pkcs1(p.read())
+    with open('keys/privateKey.pem', 'rb') as p:
+        privateKey = rsa.PrivateKey.load_pkcs1(p.read())
+    return privateKey, publicKey
+
+def encrypt(message, key):
+    return rsa.encrypt(message.encode('ascii'), key)
+
+def decrypt(ciphertext, key):
+    try:
+        return rsa.decrypt(ciphertext, key).decode('ascii')
+    except:
+        return False
+
+generateKeys()
+privateKey, publicKey = loadKeys()
+
+message = input('Write your message here:')
+ciphertext = encrypt(message, publicKey)
+
+print(f'Cipher text: {ciphertext}')
+
+text = decrypt(ciphertext, privateKey)
+
+if text:
+    print(f'Message text: {text}')
+else:
+    print(f'Unable to decrypt the message.')
